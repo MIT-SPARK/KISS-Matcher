de33232935d4bc88a4f352454e2a720a9836038b
Hyungtae Lim <shapelim@mit.edu>
2025-02-19 08:57:09 -0500

    Initialize pybind

--- python/kiss_matcher/pybind/kiss_matcher_pybind.cpp
+++ python/kiss_matcher/pybind/kiss_matcher_pybind.cpp
@@ -32,44 +32,82 @@
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
 
+#include "kiss_matcher/GncSolver.hpp"
 #include "kiss_matcher/KISSMatcher.hpp"
 
 #include "./stl_vector_eigen.h"
 
 namespace py = pybind11;
 using namespace py::literals;
+using namespace kiss_matcher;
 
 PYBIND11_MAKE_OPAQUE(std::vector<Eigen::Vector3d>);
 
-namespace kiss_matcher {
-PYBIND11_MODULE(kiss_matcher_pybind, m) {
-  m.doc() = "Pybind11 bindings for KISSMatcher library";
+PYBIND11_MODULE(kiss_matcher, m) {
+  m.doc()               = "Pybind11 bindings for KISSMatcher library";
+  m.attr("__version__") = "0.3.1";
 
-  auto vector3dvector = pybind_eigen_vector_of_vector<Eigen::Vector3d>(
-      m,
-      "_Vector3dVector",
-      "std::vector<Eigen::Vector3d>",
-      py::py_array_to_vectors_double<Eigen::Vector3d>);
+  // auto vector3fvector = pybind_eigen_vector_of_vector<Eigen::Vector3f>(
+  //     m,
+  //     "_Vector3fVector",
+  //     "std::vector<Eigen::Vector3f>",
+  //     py::py_array_to_vectors_float<Eigen::Vector3f>);
+
+  // auto vector3dvector = pybind_eigen_vector_of_vector<Eigen::Vector3d>(
+  //     m,
+  //     "_Vector3dVector",
+  //     "std::vector<Eigen::Vector3d>",
+  //     py::py_array_to_vectors_double<Eigen::Vector3d>);
 
   // Bind KISSMatcherConfig
+  // py::class_<KISSMatcherConfig>(m, "KISSMatcherConfig")
+  //     .def(py::init<float, bool, float, int, float, float, float, float>(),
+  //          "voxel_size"_a              = 0.3,
+  //          "use_quatro"_a              = false,
+  //          "thr_linearity"_a           = 1.0,
+  //          "num_max_corr"_a            = 5000,
+  //          "normal_r_gain"_a           = 3.0,
+  //          "fpfh_r_gain"_a             = 5.0,
+  //          "robin_noise_bound_gain"_a  = 1.0,
+  //          "solver_noise_bound_gain"_a = 0.75)
+  //     .def_readwrite("voxel_size", &KISSMatcherConfig::voxel_size_)
+  //     .def_readwrite("normal_radius", &KISSMatcherConfig::normal_radius_)
+  //     .def_readwrite("fpfh_radius", &KISSMatcherConfig::fpfh_radius_)
+  //     .def_readwrite("thr_linearity", &KISSMatcherConfig::thr_linearity_)
+  //     .def_readwrite("robin_noise_bound_gain", &KISSMatcherConfig::robin_noise_bound_gain_)
+  //     .def_readwrite("solver_noise_bound_gain", &KISSMatcherConfig::solver_noise_bound_gain_)
+  //     .def_readwrite("num_max_corr", &KISSMatcherConfig::num_max_corr_)
+  //     .def_readwrite("use_quatro", &KISSMatcherConfig::use_quatro_);
+
   py::class_<KISSMatcherConfig>(m, "KISSMatcherConfig")
-      .def(py::init<float, bool, float, int, float, float, float, float>(),
-           "voxel_size"_a              = 0.3,
-           "use_quatro"_a              = false,
-           "thr_linearity"_a           = 1.0,
-           "num_max_corr"_a            = 5000,
-           "normal_r_gain"_a           = 3.0,
-           "fpfh_r_gain"_a             = 5.0,
-           "robin_noise_bound_gain"_a  = 1.0,
-           "solver_noise_bound_gain"_a = 0.5)
+      .def(py::init<float, bool, bool, float, int, float, float, float, float, bool>(),
+           "voxel_size"_a                  = 0.3,
+           "use_voxel_sampling"_a          = true,
+           "use_quatro"_a                  = false,
+           "thr_linearity"_a               = 1.0,
+           "num_max_corr"_a                = 5000,
+           "normal_r_gain"_a               = 3.0,
+           "fpfh_r_gain"_a                 = 5.0,
+           "robin_noise_bound_gain"_a      = 1.0,
+           "solver_noise_bound_gain"_a     = 0.75,
+           "enable_noise_bound_clamping"_a = true)
       .def_readwrite("voxel_size", &KISSMatcherConfig::voxel_size_)
+      .def_readwrite("use_voxel_sampling", &KISSMatcherConfig::use_voxel_sampling_)
+      .def_readwrite("use_quatro", &KISSMatcherConfig::use_quatro_)
+      .def_readwrite("thr_linearity", &KISSMatcherConfig::thr_linearity_)
+      .def_readwrite("num_max_corr", &KISSMatcherConfig::num_max_corr_)
       .def_readwrite("normal_radius", &KISSMatcherConfig::normal_radius_)
       .def_readwrite("fpfh_radius", &KISSMatcherConfig::fpfh_radius_)
-      .def_readwrite("thr_linearity", &KISSMatcherConfig::thr_linearity_)
       .def_readwrite("robin_noise_bound_gain", &KISSMatcherConfig::robin_noise_bound_gain_)
       .def_readwrite("solver_noise_bound_gain", &KISSMatcherConfig::solver_noise_bound_gain_)
-      .def_readwrite("num_max_corr", &KISSMatcherConfig::num_max_corr_)
-      .def_readwrite("use_quatro", &KISSMatcherConfig::use_quatro_);
+      .def_readwrite("robin_noise_bound", &KISSMatcherConfig::robin_noise_bound_)
+      .def_readwrite("solver_noise_bound", &KISSMatcherConfig::solver_noise_bound_);
+
+  // Bind RegistrationSolution
+  py::class_<RegistrationSolution>(m, "RegistrationSolution")
+      .def_readwrite("valid", &RegistrationSolution::valid)
+      .def_readwrite("translation", &RegistrationSolution::translation)
+      .def_readwrite("rotation", &RegistrationSolution::rotation);
 
   // Bind KISSMatcher
   py::class_<KISSMatcher>(m, "KISSMatcher")
@@ -109,4 +147,3 @@ PYBIND11_MODULE(kiss_matcher_pybind, m) {
       .def("get_solver_time", &KISSMatcher::getSolverTime, "Get solver time")
       .def("print", &KISSMatcher::print, "Print matcher state");
 }
-}  // namespace kiss_matcher
