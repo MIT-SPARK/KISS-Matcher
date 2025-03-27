6ddd8e29c284bcd0dffa82fdae52f0d28804a9d0
Hyungtae Lim <shapelim@mit.edu>
2025-03-27 13:47:49 -0400

    Upload launch file

--- ros/launch/visualizer_launch.py
+++ ros/launch/visualizer_launch.py
@@ -5,22 +5,27 @@ import launch_ros.actions
 
 
 def generate_launch_description():
-    params_file = os.path.join(os.getcwd(),
-                               "src/KISS-Matcher/ros/config/params.yaml")
+    params_file = os.path.join(os.getcwd(), "src/KISS-Matcher/ros/config/params.yaml")
 
-    return launch.LaunchDescription([
-        launch_ros.actions.Node(package='kiss_matcher_ros',
-                                executable='registration_visualizer',
-                                name='registration_visualizer',
-                                parameters=[params_file]),
-        launch_ros.actions.Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            arguments=[
-                '-d',
-                os.path.join(os.getcwd(),
-                             "src/KISS-Matcher/ros/rviz/km_rviz.rviz")
-            ],
-            output='screen')
-    ])
+    return launch.LaunchDescription(
+        [
+            launch_ros.actions.Node(
+                package="kiss_matcher_ros",
+                executable="registration_visualizer",
+                name="registration_visualizer",
+                parameters=[params_file],
+            ),
+            launch_ros.actions.Node(
+                package="rviz2",
+                executable="rviz2",
+                name="rviz2",
+                arguments=[
+                    "-d",
+                    os.path.join(
+                        os.getcwd(), "src/KISS-Matcher/ros/rviz/kiss_matcher_reg.rviz"
+                    ),
+                ],
+                output="screen",
+            ),
+        ]
+    )
