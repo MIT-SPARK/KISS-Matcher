d7a946421829c10f842a9228a21c516d94523360
Hyungtae Lim <shapelim@mit.edu>
2025-03-30 22:31:25 -0400

    Set loop types

--- ros/include/slam/loop_candidate.hpp
+++ ros/include/slam/loop_candidate.hpp
@@ -5,11 +5,19 @@
 
 #include <cstddef>  // for size_t
 #include <limits>   // for std::numeric_limits
+#include <utility>
+#include <vector>
 
 struct LoopCandidate {
   bool found_      = false;
   size_t idx_      = std::numeric_limits<size_t>::max();
   double distance_ = std::numeric_limits<double>::max();
 };
+
+using LoopCandidates = std::vector<LoopCandidate>;
+
+using LoopIdxPair = std::pair<size_t, size_t>;
+
+using LoopIdxPairs = std::vector<LoopIdxPair>;
 
 #endif  // KISS_MATCHER_LOOP_CANDIDATE_H
